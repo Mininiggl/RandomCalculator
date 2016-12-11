b501dfed5c2dd3f9b9607f41dd7cf6c7137351b6
Mininiggl <bjoern.-_nickel@web.de>
2016-12-11 19:03:01 +0100

    Zufallszahlen angezeigt und erzeugt in zufallZahlen

--- app/src/main/java/hs_hannover/de/randomcalculator/MainActivity.java
+++ app/src/main/java/hs_hannover/de/randomcalculator/MainActivity.java
@@ -2,16 +2,41 @@ package hs_hannover.de.randomcalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
+    private int SummandA;
+    private int SummandB;
+    private int Summe;
+    TextView textSummandA;
+    TextView textSummandB;
+    TextView textSumme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textSummandA = (TextView) findViewById(R.id.textSummandA);
+        textSummandB = (TextView) findViewById(R.id.textSummandB);
+        textSumme = (TextView) findViewById(R.id.textSumme);
+        zufallGenerier();
     }
 
-    public void Add(){
-        int i = 1+1;
+    public void zufallGenerier(){
+        zufallZahlen(0);
     }
+
+    public void zufallZahlen(int level){
+        Random r = new Random();
+        SummandA = r.nextInt((10*(int) Math.pow(10,level)))+1;
+        textSummandA.setText(String.valueOf(SummandA));
+        SummandB = r.nextInt((10*(int) Math.pow(10,level)))+1;
+        textSummandB.setText(String.valueOf(SummandB));
+        Summe = SummandA + SummandB;
+        textSumme.setText(String.valueOf(Summe));
+    }
+
+
 }
